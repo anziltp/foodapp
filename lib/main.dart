451b11b21f5fb225f870a2ac2e8b4b6a2251f801
admin <anziltp902@gmail.com>
2024-03-12 12:46:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Splash_Screen.dart';
+
+var w;
+var h;
+
+
+Future<void> main() async {
+
+
+  runApp( FoodApp());
+}
+class FoodApp extends StatelessWidget {
+  const FoodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    w=MediaQuery.of(context).size.width;
+    h=MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus!.unfocus();
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:SplashScreen() ,
+        theme: ThemeData(
+
+        ),
+      ),
+    );
+
+  }
+}
