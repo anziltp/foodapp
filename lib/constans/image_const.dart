eb62998c78e12c278ff4228f6f86a960c37aa517
hello <fijinshifas1@gmail.com>
2024-03-14 10:58:01 +0530

    CREATE login page

--- lib/constans/image_const.dart
+++ lib/constans/image_const.dart
@@ -1,6 +1,7 @@
 class ImageConst{
 static const splashscreen="assets/images/splashscreen.png";
 static const firstpageimage="assets/images/firstpageimage.png";
+static const secondpageimage="assets/images/secondpageimage.png";
 static const threedots="assets/icons/threedots.svg";
 static const google="assets/images/google.png";
 static const googlesvg="assets/icons/googlesvg.svg";
