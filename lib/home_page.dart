8de56e565085eae4ad3557ed22bc8828d565fe8e
admin <anziltp902@gmail.com>
2024-03-15 10:39:35 +0530

    created home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import 'constans/color_const.dart';
+import 'constans/image_const.dart';
 import 'main.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  TextEditingController name = TextEditingController();
+  TextEditingController password = TextEditingController();
   PageController pageController = PageController();
   SideMenuController sideMenu = SideMenuController();
 
@@ -106,7 +110,7 @@ sideMenu.changePage(index);
         // Page controller to manage a PageView
         controller: sideMenu,
         // Will shows on top of all items, it can be a logo or a Title text
-        title: Image.network("https://play-lh.googleusercontent.com/_CpzRzAeTMMAooQFMnAtbVTQXs8Av41CFlVqwZ5zbvi-5G3vMsL5InfRgMdOY24SqQ",width: w*0.15,),
+        title: Image.network("https://raw.githubusercontent.com/Jamalianpour/easy_sidemenu/master/images/logo.png",width: w*0.15,),
     // Will show on bottom of SideMenu when displayMode was SideMenuDisplayMode.open
     footer: Text('Food App'),
     // Notify when display mode changed
@@ -117,25 +121,72 @@ sideMenu.changePage(index);
     items: items,
     ),
     Expanded(
-    child: PageView(
-    controller: pageController,
-    children: [
-    Container(
-    child: Center(
-    child: Text('Dashboard'),
-    ),
-    ),
-    Container(
-    child: Center(
-    child: Text('Settings'),
-    ),
-    ),Container(
-    child: Center(
-    child: Text('menu'),
-    ),
-    ),
-    ],
-    ),
+    child:Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Center(child: Image.asset(ImageConst.splashscreen,width: w*0.09,)),
+              Column(
+                children: [
+                  Container(
+                    width: w * 0.2,
+                    height: w * 0.03,
+                    decoration: BoxDecoration(
+                      // border: Border.all(color: Colorconst.primarycolor),
+                      // borderRadius:
+                      color: ColorConst.white,
+                    ),
+                    child: TextFormField(
+                        controller: name,
+                        keyboardType: TextInputType.name,
+                        autovalidateMode:
+                        AutovalidateMode.onUserInteraction,
+                        textInputAction: TextInputAction.newline,
+                        decoration: InputDecoration(
+
+                          label: Text("User Name",
+                            style: TextStyle(
+                                color: ColorConst.black,
+                                fontSize: w * 0.01),),
+                          focusedBorder:  OutlineInputBorder(borderRadius: BorderRadius.circular(w * 0.04),),
+                          enabledBorder:  OutlineInputBorder(borderRadius: BorderRadius.circular(w * 0.04),),
+                        )),
+                  ),
+                  SizedBox(height: h*0.05,),
+                  Container(
+                    width: w * 0.2,
+                    height: w * 0.03,
+                    decoration: BoxDecoration(
+                      // border: Border.all(color: Colorconst.primarycolor),
+                      // borderRadius:
+                      color: ColorConst.white,
+                    ),
+                    child: TextFormField(
+                        controller: password,
+                        keyboardType: TextInputType.text,
+                        autovalidateMode:
+                        AutovalidateMode.onUserInteraction,
+                        textInputAction: TextInputAction.newline,
+                        decoration: InputDecoration(
+
+                          label: Text("password",
+                            style: TextStyle(
+                                color: ColorConst.black,
+                                fontSize: w * 0.01),),
+                          focusedBorder:  OutlineInputBorder(borderRadius: BorderRadius.circular(w * 0.04),),
+                          enabledBorder:  OutlineInputBorder(borderRadius: BorderRadius.circular(w * 0.04),),
+                        )),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    )
     ),
           SideMenuItem(
             builder: (context, displayMode) {
