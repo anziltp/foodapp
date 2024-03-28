6dd94d7a9757d67489ee74dfc853e00afa6440cf
admin <anziltp902@gmail.com>
2024-03-28 11:03:09 +0530

    updated side menu

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,210 +1,127 @@
-// class onBodyContent{
-//   String image;
-//   String text;
-//   onBodyContent({required this.image,required this.text});
-// }
-// List<onBodyContent>contents=[
-//   onBodyContent(image:( "assets/images/firstpageimage.png"),
-//       text: "Order from your favourite\nstores or vendors"),
-//       onBodyContent(image: ("assets/images/firstpageimage2.png"), text: "Choose from a wide range of \n delicious meals"),
-//   onBodyContent(image: ("assets/images/firstpageimage3.png"), text: "Enjoy instant delivery \nand payment")
-// ];
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_side_menu/flutter_side_menu.dart';
-import 'package:foodapp/constans/image_const.dart';
-
-
-import 'package:foodapp/login%20part/create_account.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foodapp/constans/color_const.dart';
 import 'package:foodapp/sidemenu%20part/add_items.dart';
 import 'package:foodapp/sidemenu%20part/booking.dart';
 import 'package:foodapp/sidemenu%20part/dashboard.dart';
 import 'package:foodapp/sidemenu%20part/product%20_Adding/product_Home.dart';
-
 import 'package:foodapp/sidemenu%20part/users.dart';
+import 'package:side_navigation/side_navigation.dart';
 
-
-import 'constans/color_const.dart';
+import 'constans/image_const.dart';
 import 'main.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-TextEditingController name = TextEditingController();
-TextEditingController password = TextEditingController();
-TextEditingController email = TextEditingController();
 class _HomePageState extends State<HomePage> {
-  final _controller = SideMenuController();
-  int _currentIndex = 0;
+  /// Views to display
   List<Widget> views = const [
    Dashbored(),
     Users(),
     Bookings(),
     AddItems(),
-    ProductHome()
-
+    ProductHome(),
   ];
+
+  /// The currently selected index of the bar
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      /// You can use an AppBar if you want to
       appBar: AppBar(
         backgroundColor: Colors.grey,
-title: const Text("hello,  user",style: TextStyle(fontWeight: FontWeight.w700),),
+        title: Center(child: const Text('Food App')),
       ),
-body: Row(
-  children: [
-    SideMenu(
-      controller: _controller,
-      backgroundColor: Colors.white,
-priority: SideMenuPriority.mode,
-      mode: SideMenuMode.open,
-      builder: (data) {
-        return SideMenuData(
 
-          header: Container(
-            height: h*0.2,
+      // The row is needed to display the current view
+      body: Row(
+        children: [
+          /// Pretty similar to the BottomNavigationBar!
+          SideNavigationBar(
 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(ImageConst.splashscreen,width: w*0.05,),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Food App",style: TextStyle(fontWeight: FontWeight.w700,color: ColorConst.primerycolor,fontSize: w*0.012),),
-                    Text("Admin",style: TextStyle(fontWeight: FontWeight.w700,color: Colors.grey),),
-                  ],
-                )
+            selectedIndex: selectedIndex,
+            footer: SideNavigationBarFooter(label: InkWell(
+              onTap: () {
+                showCupertinoModalPopup(context: context, builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: const Text("Are you sure\nYou want to Exit"),
+                    actions: [
+                      Column(
+                        children: [
+                          CupertinoDialogAction(child: Text("yes",style: TextStyle(fontSize: w*0.01,fontWeight: FontWeight.w800,color: Colors.red),)),
+                          const Divider(),
+                          CupertinoDialogAction(child: Text("cancel",style: TextStyle(fontSize: w*0.01,fontWeight: FontWeight.w800),)
+                            ,onPressed: () {
+                              Navigator.pop(context);
+                            },),
 
-              ],
-            ),
-          ),
-          items: [
+                        ],
+                      )
+                    ],
+                  );
+                },);
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Exit",style: TextStyle(fontWeight: FontWeight.w700,fontSize: w*0.015),),
+                  SizedBox(width: w*0.01,),
+                  Icon(Icons.exit_to_app)
+                ],
+              ),
+            )),
+            header: SideNavigationBarHeader(image: Image.asset(ImageConst.splashscreen,width: w*0.07,), title:  Text("Yum Yard",style: TextStyle(fontWeight: FontWeight.w700,fontSize: w*0.012,color: ColorConst.primerycolor),), subtitle: Padding(
+              padding:  EdgeInsets.only(left:w*0.01),
+              child: const Text("admin",style: TextStyle(fontWeight: FontWeight.w700,color: Colors.grey),),
+            )),
+            items: const [
 
-            SideMenuItemDataTile(
-              isSelected: _currentIndex == 0,
-              onTap: () => setState(() => _currentIndex = 0),
-              title: ' Dashboard',
-              hoverColor: Colors.blueGrey,
-              selectedTitleStyle:
-              const TextStyle(fontWeight: FontWeight.w700,color: Colors.black),
-              titleStyle: const TextStyle(color: Colors.black),
-              icon: const Icon(Icons.home_outlined),
-              selectedIcon: const Icon(Icons.home),
+              SideNavigationBarItem(
 
-            ),
+                icon: Icons.dashboard,
+                label: 'Dashboard',
 
-            SideMenuItemDataTile(
-              isSelected: _currentIndex == 1,
-              onTap: () => setState(() => _currentIndex = 1),
-              title: 'Users',
-              hoverColor: Colors.blueGrey,
-              selectedTitleStyle:
-              const TextStyle(fontWeight: FontWeight.w700,color: Colors.black),
-              icon: const Icon(Icons.person),
-              selectedIcon: const Icon(Icons.person),
-              titleStyle: const TextStyle(color: Colors.black),
-            ),
+              ),
+              SideNavigationBarItem(
+                icon: Icons.person,
+                label: 'users',
 
-            SideMenuItemDataTile(
-              isSelected: _currentIndex == 2,
-              onTap: () => setState(() => _currentIndex = 2),
-              title: 'Bookings',
-              hoverColor: Colors.blueGrey,
-              selectedTitleStyle:
-              const TextStyle(fontWeight: FontWeight.w700,color: Colors.black),
-              icon: const Icon(Icons.shopping_cart),
-              selectedIcon: const Icon(Icons.shopping_cart),
-              titleStyle: const TextStyle(color: Colors.black),
-            ),
-            SideMenuItemDataTile(
-              isSelected: _currentIndex == 3,
-              onTap: () => setState(() => _currentIndex = 3),
-              title: 'Items',
-              hoverColor: Colors.blueGrey,
-              selectedTitleStyle:
-              const TextStyle(fontWeight: FontWeight.w700,color: Colors.black),
-              icon: const Icon(Icons.import_export),
-              selectedIcon: const Icon(Icons.import_export),
-              titleStyle: const TextStyle(color: Colors.black),
-            ),
-            SideMenuItemDataTile(
-              isSelected: _currentIndex == 4,
-              onTap: () => setState(() => _currentIndex = 4),
-              title: 'Products',
-              hoverColor: Colors.blueGrey,
-              selectedTitleStyle:
-              const TextStyle(fontWeight: FontWeight.w700,color: Colors.black),
-              icon: const Icon(Icons.production_quantity_limits),
-              selectedIcon: const Icon(Icons.production_quantity_limits),
-              titleStyle: const TextStyle(color: Colors.black),
-            ),
+              ),
+              SideNavigationBarItem(
+                icon: Icons.production_quantity_limits_outlined,
+                label: 'Bookings',
+              ),
+              SideNavigationBarItem(
+                icon: Icons.import_export,
+                label: 'Items',
+              ),
+              SideNavigationBarItem(
+                icon: Icons.production_quantity_limits,
+                label: 'Products',
+              ),
 
-
-          ],
-          footer: InkWell(
-            onTap: () {
-              showCupertinoModalPopup(context: context, builder: (context) {
-                return CupertinoAlertDialog(
-                  title: const Text("Are you sure\nYou want to Exit"),
-                  actions: [
-                    Column(
-                      children: [
-                        CupertinoDialogAction(child: Text("yes",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w800,color: Colors.red),)),
-                        const Divider(),
-                        CupertinoDialogAction(child: Text("cancel",style:TextStyle(fontSize: 17,fontWeight: FontWeight.w800),)
-                          ,onPressed: () {
-                            Navigator.pop(context);
-                          },),
-
-                      ],
-                    )
-                  ],
-                );
-              },);
+            ],
+            onTap: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
             },
-            child: Container(
-              height: h*0.07,
-              width: w*0.2,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                color: Colors.white,
-              ),
-
-              child: Padding(
-                padding:  EdgeInsets.only(left:w*0.04),
-                child: Row(
-
-                  children: [
-                    Text('EXIT',style: TextStyle(
-                      fontWeight: FontWeight.w700,fontSize: w*0.02
-                    ),),
-                    SizedBox(width: w*0.016,),
-                    const Icon(Icons.exit_to_app,)
-                  ],
-                ),
-              ),
-            ),
           ),
-        );
-      },
-    ),
-    Expanded(
-      flex: 3,
-      child:  views.elementAt(_currentIndex),
-    ),
 
-  ],
-),
-
-
+          /// Make it take the rest of the available width
+          Expanded(
+            child: views.elementAt(selectedIndex),
+          )
+        ],
+      ),
     );
-
   }
 }
