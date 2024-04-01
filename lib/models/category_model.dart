706236c79a0e29a25cf3e3246760f2132008a323
hp <mohammedyaseen.k46@gmail.com>
2024-04-01 11:37:04 +0530

    add stream

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,22 +1,27 @@
 class CategoryModel {
   String category;
   String image;
+  String id;
 
-  CategoryModel({required this.category, required this.image});
+  CategoryModel(
+      {required this.category, required this.image, required this.id});
 
   Map<String, dynamic> toMap() {
-    return {"category": this.category, "image": this.image};
+    return {"category": this.category, "image": this.image, "id": this.id};
   }
 
   factory CategoryModel.fromMap(Map<String, dynamic> map) {
     return CategoryModel(
       category: map["category"] ?? "",
       image: map["image"] ?? "",
+      id: map["id"] ?? "",
     );
   }
 
-  CategoryModel copyWith({String? category, String? image}) {
+  CategoryModel copyWith({String? category, String? image, String? id}) {
     return CategoryModel(
-        category: category ?? this.category, image: image ?? this.image);
+        category: category ?? this.category,
+        image: image ?? this.image,
+        id: id ?? this.id);
   }
 }
